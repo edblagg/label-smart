e482910a32604b64ee3fb46354a6dbcb679ed42b
ERIC BLAGG, CPA, CMA <33696877+edblagg@users.noreply.github.com>
2022-01-05 10:31:32 -0500

    Update senko.py

--- main/senko.py
+++ main/senko.py
@@ -6,7 +6,7 @@ class Senko:
     raw = "https://raw.githubusercontent.com"
     github = "https://github.com"
 
-    def __init__(self, user, repo, url=None, branch="main", working_dir="main", files=["main.py"], headers={}):
+    def __init__(self, user, repo, url=None, branch="master", working_dir="main", files=["main.py"], headers={}):
         """Senko OTA agent class.
 
         Args:
